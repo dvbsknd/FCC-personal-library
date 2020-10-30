626449fa05c2c92a86e3620e5e1d63b5ad449564
Dave Baskind <dave@picketstudio.com>
2020-10-30 11:36:59 +1100

    Ongoing compontent test set-up

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -7,7 +7,8 @@ export default function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('/api/books')
+    console.log('HOST', window.location.host);
+    fetch(window.location.host + '/api/books')
       .then(response => response.json())
       .then(data => {
         setData(data);
--- client/components/App/test.js
+++ client/components/App/test.js
@@ -3,14 +3,23 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const { act } = require('react-dom/test-utils');
-const Component = require('./index.js');
+const { JSDOM } = require("jsdom");
+const dom = new JSDOM(``);
+const fetch = require('node-fetch');
+const fetchMock = require('fetch-mock');
+const expect = require('chai').expect;
+import App from './';
 
 describe('<App>', () => {
 
   let container;
+  global.window = dom.window;
+  let document = window.document;
+  global.fetch = fetch;
 
   beforeEach(() => {
     container = document.createElement('div');
+    container.setAttribute('id', 'root');
     document.body.appendChild(container);
   });
 
@@ -22,9 +31,8 @@ describe('<App>', () => {
   it('Renders the main App component and site title',() => {
 
     act(() => {
-      ReactDOM.render(<Component />, container);
+      ReactDOM.render(<App />, container);
     });
-    
     const header = container.querySelector('h1.header');
     expect(header.textContent).toBe('My Books');
   });
