91c83e534a37d666a0a5506bfa5a2222bdd045df
Dave Baskind <dave@picketstudio.com>
2020-11-04 08:49:25 +1100

    Recommit layout adjustments (git mistake)

--- client/components/BookList/index.js
+++ client/components/BookList/index.js
@@ -3,10 +3,10 @@ import { Card } from 'semantic-ui-react';
 
 export default function BookList(props) {
   return (
-    <Card.Group>
+    <Card.Group itemsPerRow={3}>
       {props.books.map(book => {
         return (
-          <Card key={book._id}>
+          <Card key={book._id} fluid={true}>
             <Card.Content>
               <Card.Header className='bookTitle'>{book.title}</Card.Header>
               <Card.Meta className='bookAuthor'>{book.author}</Card.Meta>
