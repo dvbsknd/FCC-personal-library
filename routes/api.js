7f3cded5531761dfef57d73a9ae6863924cc9722
Dave Baskind <dave@picketstudio.com>
2020-11-30 17:46:58 +1100

    Add a  route handler

---
+++ routes/api.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const api = express.Router();
+const {
+  booksController
+} = require('../controllers');
+
+api.use((req, res, next) => {
+  res.error = (error) => {
+    res.status(400);
+    res.json({ error: error.message });
+  };
+  next();
+});
+
+api.use(express.json());
+
+api.route('/books')
+  .get((req, res) => {
+    booksController.list()
+      .then(result => res.json(result))
+      .catch(res.error);
+  })
+  .post((req, res) => {
+    booksController.add(req.body.title, req.body.author)
+      .then(result => res.json(result))
+      .catch(res.error);
+  })
+  .delete((req, res) => {
+    booksController.delete(req.body._id)
+      .then(result => res.json(result))
+      .catch(res.error);
+  });
+
+api.route('/comments')
+  .post((req, res) => {
+    booksController.addComment(req.body.bookId, req.body.comment)
+      .then(result => res.json(result))
+      .catch(res.error);
+  })
+  .delete((req, res) => {
+    booksController.deleteComment(req.body._id)
+      .then(result => res.json(result))
+      .catch(res.error);
+  });
+
+// Unmatched routes
+api.use((req, res) => {
+  res.error({ error: 'Unknown route'});
+});
+
+module.exports = api;
--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,9 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
-const {
-  booksController
-} = require('../controllers');
+const api = require('./api');
+const monitor = require('./monitor');
 
-router.use((req, res, next) => {
-  res.error = (error) => {
-    res.status(400);
-    res.json({ error: error.message });
-  };
-  next();
-});
-
-router.use(express.json());
-
-router.route('/books')
-  .get((req, res) => {
-    booksController.list()
-      .then(result => res.json(result))
-      .catch(res.error);
-  })
-  .post((req, res) => {
-    booksController.add(req.body.title, req.body.author)
-      .then(result => res.json(result))
-      .catch(res.error);
-  })
-  .delete((req, res) => {
-    booksController.delete(req.body._id)
-      .then(result => res.json(result))
-      .catch(res.error);
-  });
-
-router.route('/comments')
-  .post((req, res) => {
-    booksController.addComment(req.body.bookId, req.body.comment)
-      .then(result => res.json(result))
-      .catch(res.error);
-  })
-  .delete((req, res) => {
-    booksController.deleteComment(req.body._id)
-      .then(result => res.json(result))
-      .catch(res.error);
-  });
-
-// Unmatched routes
-router.use((req, res) => {
-  res.error({ error: 'Unknown route'});
-});
-
-module.exports = router;
+module.exports = {
+  api,
+  monitor
+};
